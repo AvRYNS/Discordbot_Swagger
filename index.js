3091d7628a4bf1f9a89863c8681e5874f836d3d9
AvRYNS <imath991016@naver.com>
2024-05-24 18:14:46 +0900

    v0.0.2

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 require('dotenv').config()
 
 const axios = require('axios');
-const { Client, GatewayIntentBits } = require('discord.js');
+const { Client, GatewayIntentBits, EmbedBuilder } = require('discord.js');
 const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent] });
 
 
@@ -11,19 +11,31 @@ client.on('ready', ()=> {
 })
 
 client.on('messageCreate', async (message) => {
+    if (message.content === '임베딩'){
+        const embed = new EmbedBuilder()   
+            .setColor('#AD1457')
+            .setTitle('title')
+            .setDescription('본문에 해당하는 내용이 들어갑니다.')
+            .setFooter({text: 'swagger', iconURL:'https://pbs.twimg.com/media/GN-VJvvboAAwgS_.jpg'})
+            .setThumbnail('https://pbs.twimg.com/media/GN-VJvvboAAwgS_.jpg')
+            .addFields(
+                { name: 'Field 1', value: '윤두창', inline: true },
+                { name: ' ', value: '=', inline: true },
+                { name: 'Field 3', value: '개새끼', inline: false }
+            );
+        message.channel.send({ embeds:[embed] });
+    }
     if (message.content === 'ping'){
         message.reply({
             content: 'pong',
         })
     }
-    else if (message.content === 'quote'){
-        let resp = await axios.getAdapter('https://api.quotable.io/random');
-        const quote = resp.data.content;
-
+    if (message.content === '우'){
         message.reply({
-            content: quote,
+            content: '흥',
         })
     }
+
 })
 
 client.login(process.env.DISCORD_BOT_ID)
